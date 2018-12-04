85401937364aa979367e0df5f4946069eecee871
nnovadmin <nnovadmin@NNOVADMIN>
2018-12-04 13:08:53 +0300

    Добавьте файлы проекта.

---
+++ OSLab8/OSLab8.py
@@ -0,0 +1,47 @@
+# Уровень вложенности через подсчет слешей
+import win32con
+import win32api
+import os
+
+# limits for recursive walking
+lower_limit = 2
+upper_limit = 4
+# Only one file extension should be used
+file_extension = 'docx'
+
+go_dir = input('Input dir name:')
+folder = []
+FileList= []
+for i in os.walk(go_dir):
+    folder.append(i)
+# all files
+print('=== Show all files ===')
+for address, dirs, files in folder:
+    for file in files:
+        print(address+'/'+file)
+# all hidden files
+print('=== Show all hidden files ===')
+for address, dirs, files in folder:
+    for file in files:
+        if win32api.GetFileAttributes(address+'/'+file) & win32con.FILE_ATTRIBUTE_HIDDEN:
+            print(address+'/'+file)
+# only <file_ext> hidden files
+print('=== Show only <' + file_extension + '> hidden files ===')
+for address, dirs, files in folder:
+    for file in files:
+        if win32api.GetFileAttributes(address+'/'+file) & win32con.FILE_ATTRIBUTE_HIDDEN:
+            if file.endswith('.'+file_extension):
+                print(address+'/'+file)
+# files, those exists between recursive walking limits
+print('=== Show only <' + file_extension + '> hidden files in walking limit ===')
+for address, dirs, files in folder:
+    for file in files:
+        if win32api.GetFileAttributes(address+'/'+file) & win32con.FILE_ATTRIBUTE_HIDDEN:
+            if ((address.count('\\')+1) >= lower_limit) & ((address.count('\\')+1) <= upper_limit):
+                if file.endswith('.'+file_extension):
+                    print(address+'/'+file)
+                    FileList.append(file)
+print ('=== File names: ===')
+print ('Unsorted alphabeticaly: \n' + str(FileList))
+FileList.sort()
+print ('Sorted alphabeticaly: \n' + str(FileList))
